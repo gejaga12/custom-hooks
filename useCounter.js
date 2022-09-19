da08018bfc961419f47bd7cdece33e658a13660b
Gerardo Javier García <gejaga12@hotmail.com>
2022-09-19 15:16:18 -0300

    HooksPersonalizados

---
+++ useCounter.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue = 0) => {
+  const [counter, setcounter] = useState(initialValue);
+
+  const increment = (value = 1) => {
+    setcounter(counter + value);
+  };
+
+  const decrement = (value = 1) => {
+    if (counter === 1 ) return;
+    setcounter(counter - value);
+  };
+
+  const reset = () => {
+    setcounter(initialValue);
+  };
+  
+  return {
+    counter,
+    increment,
+    reset,
+    decrement,
+  };
+};
